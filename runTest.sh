baeefb3d29593ec991650a9ce8c46884e399208b
Neena Pandey <neenapandey7@gmail.com>
2023-08-03 12:10:09 +1000

    .

--- runTest.sh
+++ runTest.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 newman -v
 newman run collection/Swagger_Petstore_slacknotifi_final.postman_collection.json -d data/petstore_order.csv -e env/qa.postman_environment.json -r cli,htmlextra --reporter-htmlextra-export "newman/newman_result.html"
+#a/petstore_order.csv -e env/qa.postman_environment.json -r cli,htmlextra --reporter-htmlextra-export "newman/newman_result.html"
